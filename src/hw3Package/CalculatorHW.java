8a38f894ec7f405ebd221602daed4b1f2d9e5359
Garrad Belle <garrad.d.belle@gmail.com>
2021-10-04 23:55:01 -0400

    first commit

---
+++ src/hw3Package/CalculatorHW.java
@@ -0,0 +1,21 @@
+package hw3Package;
+
+import java.util.Scanner;
+
+public class CalculatorHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Write a program to create a calculator that can Add, Subtract, Multiply,
+		// and Divide 2 numbers entered by the user. Also, add an option to exit out from the program
+		
+		Scanner userInput1 = new Scanner(System.in);
+		Scanner userInput2 = new Scanner(System.in);
+		System.out.println("Please enter two numbers.");
+		
+		float userNum1 = userInput1.nextFloat();
+		float userNum2 = userInput2.nextFloat();
+	}
+
+}
